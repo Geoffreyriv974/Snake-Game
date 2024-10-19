738c03de2f3f544c11addaada4aa43b1ce97b7b3
Geoffrey <geoffrey.riviere@ecole-it.com>
2024-10-19 23:15:54 +0200

    Ajout du fichier du menu principal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from game import *
+from score import *
 
 
 def main():
@@ -9,13 +10,15 @@ def main():
 
     menu = tk.Tk()
     menu.title('Menu Snake Games')
+    menu.geometry('500x500')
 
-    canvas = tk.Canvas(menu, width=500, height=500, bg='black')
-    canvas.pack()
-    canvas.create_text(WIDTH / 2, HEIGHT / 4, text="SNAKE GAME", fill="green", font=("Arial", 24))
+    Game_title = tk.Label(menu, fg="green", text='Snake-Games', font=('Arial', 30))
+    Game_title.pack(ipady=100)
 
-    btn_start = tk.Button(menu, text="START", command=start_game, font=("Arial", 24), cursor="hand2")
-    btn_start.pack()
+    btn_start = tk.Button(menu, text="START", command=start_game, font=("Arial", 24), cursor="hand2", borderwidth=10)
+    btn_start.pack(side=tk.LEFT, padx=50)
+    btn_score = tk.Button(menu, text="SCORE", font=("Arial", 24), cursor="hand2", borderwidth=10)
+    btn_score.pack(side=tk.RIGHT, padx=50)
 
     menu.mainloop()
 
