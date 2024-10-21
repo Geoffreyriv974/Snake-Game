27e0b8661c020237f61756c3b0647a1b4e7ad615
Geoffrey <geoffrey.riviere@ecole-it.com>
2024-10-22 01:46:45 +0200

    relation fichier mode

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 from game import *
 from score import *
-
+from mode import *
 
 def main():
 
-    def start_game():
-        game()
+    def select_mode():
+        choice_mode()
 
     menu = tk.Tk()
     menu.title('Menu Snake Games')
@@ -15,10 +15,22 @@ def main():
     Game_title = tk.Label(menu, fg="green", text='Snake-Games', font=('Arial', 30))
     Game_title.pack(ipady=100)
 
-    btn_start = tk.Button(menu, text="START", command=start_game, font=("Arial", 24), cursor="hand2", borderwidth=10)
+    btn_start = tk.Button(menu, text="START", command=select_mode, font=("Arial", 24), cursor="hand2", borderwidth=10)
     btn_start.pack(side=tk.LEFT, padx=50)
+
     btn_score = tk.Button(menu, text="SCORE", font=("Arial", 24), cursor="hand2", borderwidth=10)
     btn_score.pack(side=tk.RIGHT, padx=50)
+
+    # image_pomme = Image.open("image/pomme.png")
+    # image_pomme = image_pomme.resize((20, 20))
+
+    # pommes = ImageTk.PhotoImage(image_pomme)
+    # canvas = tk.Canvas(menu, width=500, height=500)
+    # canvas.create_image(500/2, 500/2, image=pommes)
+
+    # canvas.image = pommes
+
+    # canvas.pack()
 
     menu.mainloop()
 
